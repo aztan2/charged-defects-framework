aac0d9b13ad466641e06678c299c8762f658171c
Anne Marie Tan <annemarietan@ufl.edu>
2019-08-29 16:30:46 -0400

    Modify gen_defect_supercells to read defect type, site from initdef.json file provided by user. Imploemented for vacancy and substitutional defects.

--- gen_defect_supercells.py
+++ gen_defect_supercells.py
@@ -1,6 +1,7 @@
 import os
 import json
 import argparse
+import itertools
 import numpy as np
 import warnings
 with warnings.catch_warnings():
@@ -20,6 +21,26 @@ class Defect():
         self.defect_type = []
         self.defect_site = []
             
+
+    def get_defect_site(self, initdef):
+
+        ## figuring out min and max+1 indices for each species        
+        syms = [site.specie.symbol for site in self.structure]
+        elems = [el[0] for el in itertools.groupby(syms)]
+        elem_max = np.cumsum([len(tuple(el[1])) for el in itertools.groupby(syms)])
+        elem_min = [0] + [i for i in elem_max[:-1]]
+        elem_minmax = {el: [imin,imax] for el,imin,imax in zip(elems,elem_min,elem_max)}
+        
+        if initdef["type"][0] == "v" or initdef["type"][0] == "s":
+            ## initdef["index"] is a relative site index
+            ## if possible, figure out a less clunky way to index...
+            if initdef["index"] < 0:
+                defect_site = structure_bulk[int(elem_minmax[initdef["species"]][1]+initdef["index"])]
+            else:
+                defect_site = structure_bulk[int(elem_minmax[initdef["species"]][0]+initdef["index"])]
+            
+        return defect_site
+    
         
     def add_defect_info(self, defect_type, defect_site):
         
@@ -45,7 +66,6 @@ class Defect():
     def replace_atom(self):
      
         for def_type,def_site in zip(self.defect_type,self.defect_site):
-            print (def_type,def_site)
             if def_type[0] == "s":
                 if def_site in self.structure:
                     defect_index = self.structure.index(def_site)
@@ -74,6 +94,7 @@ if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(description='Generate defect supercells.')
     parser.add_argument('dir_poscars',help='path to directory with unitcell POSCARs')
+    parser.add_argument('json_initdef',help='json file with details to initialize defect')
     parser.add_argument('q',type=int,help='charge')
     parser.add_argument('supercell',help='supercell size')
     parser.add_argument('vacuum',type=int,help='vacuum spacing')
@@ -86,9 +107,10 @@ if __name__ == '__main__':
     ## the bash script already put us in the appropriate subdirectory
     dir_sub = os.getcwd()
     
+    
     ## read in corresponding unit cell POSCAR
     poscar = Poscar.from_file(os.path.join(args.dir_poscars,"POSCAR_vac_%d"%args.vacuum),
-                              check_for_POTCAR=False, read_velocities=False)       
+                              check_for_POTCAR=False, read_velocities=False)     
     
     ## make undefected supercell
     structure = poscar.structure.copy()
@@ -96,27 +118,32 @@ if __name__ == '__main__':
     structure.make_supercell([nvecs[0],nvecs[1],nvecs[2]])
     structure_bulk = structure.copy()
     
+    
+    ## read in defect details from initdefect json file
+    with open(args.json_initdef, 'r') as file:
+        initdef = json.loads(file.read())
+
     ## initialize defect object
     defect = Defect(structure.copy(),supercell=nvecs,vacuum=args.vacuum,charge=args.q)
-        
-    ## create vacancy defect
-    ## THIS PART IS STILL HARDCODED !!
-#    defect_site = structure_bulk[structure_bulk.num_sites-1]
-#    defect.add_defect_info(defect_type="vac_S",defect_site=defect_site)
-    defect_site = structure_bulk[int(structure_bulk.num_sites/3-1)]
-    defect.add_defect_info(defect_type="vac_W",defect_site=defect_site)
-    ## create second vacancy defect (divacancy)
-#    defect_site = structure_bulk[int(2*structure_bulk.num_sites/3-1)]
-#    defect_site = structure_bulk[structure_bulk.num_sites-2]
-#    defect.add_defect_info(defect_type="vac_Se",defect_site=defect_site)
+
+    ## set the defect info (type, site, species) for each defect listed in the json file
+    for d in initdef:
+        defect_site = defect.get_defect_site(initdef[d])
+        if initdef[d]["type"][0] == "v": 
+            ## vacancy type defect
+            defect.add_defect_info(defect_type=initdef[d]["type"]+"_"+initdef[d]["species"],
+                                   defect_site=defect_site)
+        if initdef[d]["type"][0] == "s": 
+            ## substitutional type defect
+            defect.add_defect_info(defect_type=initdef[d]["type"]+"_"+initdef[d]["species_new"],
+                                   defect_site=defect_site)            
+#        defect_site = structure_bulk[int(2*structure_bulk.num_sites/3-1)]
+
+    ## create vacancy defect(s)           
     defect.remove_atom()
+    ## create substitutional defect(s)
+    defect.replace_atom()
         
-    ## create substitutional defect
-#    defect_site = structure_bulk[int(structure_bulk.num_sites/3-1)]
-#    defect.add_defect_info(defect_type="sub_Nb",defect_site=defect_site)
-#    defect_site = structure_bulk[int(structure_bulk.num_sites-1)]
-#    defect.add_defect_info(defect_type="sub_O",defect_site=defect_site)
-#    defect.replace_atom()
 
     ## write POSCAR
     Poscar.write_file(Poscar(defect.structure.get_sorted_structure()),os.path.join(dir_sub,"POSCAR"))
@@ -132,53 +159,6 @@ if __name__ == '__main__':
     with open(os.path.join(dir_sub,"defectproperty.json"), 'w') as file:
          file.write(json.dumps(defect.as_dict(),indent=4)) # use `json.loads` to do the reverse
     
-    
-##    dir_main = "mp-2815_MoS2/test/GGA/mag/"
-#    dir_main = "Y:/MoS2/monolayer_Nbsub/GGA/mag/"
-#    if not os.path.exists(dir_main):
-#        os.makedirs(dir_main)
-#
-#
-#    for vac in [15,20]:
-#        ## read in corresponding unit cell POSCAR
-#        poscar = Poscar.from_file("mp-2815_MoS2/monolayer/GGA/mag/vac_%d/POSCAR"%vac)
-##        poscar = Poscar.from_file("mp-2815_MoS2/monolayer/GGA/mag/vac_%d/POSCAR_orth"%vac)
-#            
-#        for nvecs in [[4,4,1],[5,5,1]]:   
-#            ## make undefected supercell
-#            structure = poscar.structure.copy()
-#            structure.make_supercell([nvecs[0],nvecs[1],nvecs[2]])
-#            structure_bulk = structure.copy()
-#                
-#            for q in [-2]:
-#                ## create the subdirectory
-#                dir_sub = dir_main+"charge_%d/%dx%dx%d/vac_%d/"%(q,nvecs[0],nvecs[1],nvecs[2],vac)
-#                if not os.path.exists(dir_sub):
-#                    os.makedirs(dir_sub)
-#
-#                ## initialize defect object
-#                defect = Defect(structure.copy(),supercell=nvecs,vacuum=vac,charge=q)
-#                
-#                ## create S vacancy defect
-##                defect_site = structure_bulk[structure_bulk.num_sites-1]
-##                defect.add_defect_info(defect_type="vac_S",defect_site=defect_site)
-##                defect.remove_atom()
-#                
-#                ## create substitutional defect
-#                defect_site = structure_bulk[int(structure_bulk.num_sites/3-1)]
-#                defect.add_defect_info(defect_type="sub_Nb",defect_site=defect_site)
-#                defect.replace_atom(new_species="Nb")
-#    
-#                ## write POSCAR
-#                Poscar.write_file(Poscar(defect.structure.get_sorted_structure()),dir_sub+"POSCAR")
-#                if q == 0:
-#                    if not os.path.exists(dir_sub+"/bulkref"):
-#                        os.makedirs(dir_sub+"/bulkref")
-#                    Poscar.write_file(Poscar(structure_bulk),dir_sub+"bulkref/POSCAR")
-#  
-#                ## write json file
-#                with open(dir_sub+"defectproperty.json", 'w') as file:
-#                     file.write(json.dumps(defect.as_dict(),indent=4)) # use `json.loads` to do the reverse
 
 
     ## BULK  
