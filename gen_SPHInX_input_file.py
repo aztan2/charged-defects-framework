ae97c4f66bd85f2356510d0b2a236432744b351a
Anne Marie Tan <annemarietan@ufl.edu>
2020-03-12 16:26:20 -0400

    Added the python script version of apply_correction_loop.sh. Retain the old bash script for now for legacy pruposes.

--- gen_SPHInX_input_file.py
+++ gen_SPHInX_input_file.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import json
 import numpy as np
 import argparse
@@ -6,9 +7,7 @@ import warnings
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
     import pymatgen
-from pymatgen.core.structure import Structure
 from pymatgen.core.lattice import Lattice
-from pymatgen.io.vasp.inputs import Poscar
 
 
 def Ang_to_bohr(x):
@@ -59,23 +58,23 @@ def isolated_grp(slabmin,slabmax):
 
     return s
 
-
-if __name__ == '__main__':
-
     
-    parser = argparse.ArgumentParser(description='Generate INCAR')
+def main(args):
+    
+    ## define a main function callable from another python script
+    
+    parser = argparse.ArgumentParser(description='Generate SPHInX input file')
     parser.add_argument('eps',type=float,help='averaged dielectric constant')
     parser.add_argument('slab_d',type=float,help='corresponding slab thickness (Angstroms)')
 
       
     ## read in the above arguments from command line
-    args = parser.parse_args()
+    args = parser.parse_args(args)
     
 
-    ## the bash script already put us in the appropriate subdirectory
+    ## the main script should have put us in the appropriate subdirectory
     dir_sub = os.getcwd()
-
-#    lattice = Poscar.from_file(folder1+"POSCAR").structure.lattice                
+              
     with open(os.path.join(dir_sub,"defectproperty.json"), 'r') as file:
         defprop = json.loads(file.read())
     lattice = Lattice.from_dict(defprop["lattice"])
@@ -100,57 +99,10 @@ if __name__ == '__main__':
             os.makedirs(os.path.join(dir_sub,"correction"))
     with open(os.path.join(dir_sub,"correction","system.sx"),'w') as f:
         f.write(s)
-                        
+        
+        
+if __name__ == '__main__':
     
-    
-##    folder = "../mp-2815_MoS2/test/GGA/mag/"
-#    folder = "Y:/MoS2/monolayer_Nbsub/SCAN_vdW/mag/"
-#
-#    qs = [-2]
-#    cells = [(4,4),(5,5)]
-#    vacs = [15,20]
-#
-#
-##    eps_ave = 17.18  ## averaged "isotropic" dielectric constant (PBE)
-#    eps_ave = 16.27  ## averaged "isotropic" dielectric constant (SCAN w/o ionic)
-#    slab_d = 5.40  ## corresponding slab thickness in Angstroms 
-##    atomrad = 1.14  ## Sulphur atomic radius ~ 100pm = 1 Angstrom
-#    
-#    
-#    for q in qs:
-#        for cell in cells:
-#            for vac in vacs:
-#                
-#                folder1 = folder+"charge_%d/%dx%dx1/vac_%d/"%(q,cell[0],cell[1],vac)
-##                folder1 = folder+"charge_%d/%dx%dx1/vac_%d/ismear1/"%(q,cell[0],cell[1],vac)
-#
-##                lattice = Poscar.from_file(folder1+"POSCAR").structure.lattice                
-#                with open(folder1+"defectproperty.json", 'r') as file:
-#                    defprop = json.loads(file.read())
-#                lattice = Lattice.from_dict(defprop["lattice"])
-#                
-#                ## STRUCTURE GROUP
-#                s = structure_grp(lattice)
-#                
-#                ## SLAB GROUP
-#                ## assume slab is centered in the cell vertically
-#                slabmin = (lattice.c-slab_d)/2
-#                slabmax = (lattice.c+slab_d)/2
-#                s += slab_grp(slabmin,slabmax,eps=eps_ave*np.eye(3))
-#                
-#                ## CHARGE GROUP
-#                posZ = np.mean([def_site[2] for def_site in defprop["defect_site"]])
-#                s += charge_grp(posZ*lattice.c,q)
-#                
-#                ## ISOLATED GROUP
-#                s += isolated_grp(slabmin,slabmax)
-#    
-##                folder2 = folder + "../../../corr_sxdefectalign2d/GGA/varyeps/d3.12/%dx%dx1/vac_%d/charge_%d/"%(cell[0],cell[1],vac,q)
-#                folder2 = "Y:/MoS2/corrections/sxdefectalign2d/monolayer_Nbsub/SCAN_vdW/mag/%dx%dx1/vac_%d/charge_%d/"%(cell[0],cell[1],vac,q)
-##                folder2 = folder1
-#                if not os.path.exists(folder2):
-#                    os.makedirs(folder2)
-#                with open(folder2+"system.sx",'w') as f:
-#                    f.write(s)
-#                    
+    main(sys.argv[1:]) 
+                                            
                     
--- get_alignment_correction_2d.py
+++ get_alignment_correction_2d.py
@@ -1,37 +1,41 @@
 import os
-import argparse
-import logging
+import sys
 import time
+import argparse
+import myutils
 import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 
-if __name__ == '__main__':
-
+def main(args):
     
+    ## define a main function callable from another python script
+  
     parser = argparse.ArgumentParser(description='Estimate alignment correction.')
     parser.add_argument('vref',help='path to bulk LOCPOT file')
     parser.add_argument('vdef',help='path to defect LOCPOT file')
     parser.add_argument('encut',type=int,help='cutoff energy (eV)')
     parser.add_argument('q',type=int,help='charge (conventional units)')
+    parser.add_argument('--threshold_slope',type=float,default=1e-3,
+                        help='threshold for determining if potential is flat')
+    parser.add_argument('--threshold_C',type=float,default=1e-3,
+                        help='threshold for determining if potential is aligned')
     parser.add_argument('--vfile',help='vline .dat file',default='vline-eV.dat')
     parser.add_argument('--noplot',help='do not generate plots',default=False,action='store_true')
     parser.add_argument('--logfile',help='logfile to save output to')
        
     ## read in the above arguments from command line
-    args = parser.parse_args()
+    args = parser.parse_args(args)
+    
     
     ## set up logging
     if args.logfile:
-        logging.basicConfig(filename=args.logfile,filemode='w',
-                            format='%(levelname)s:%(message)s',level=logging.DEBUG)    
-        console = logging.StreamHandler()
-        console.setLevel(logging.INFO)
-        console.setFormatter(logging.Formatter('%(levelname)s:%(message)s'))
-        logging.getLogger('').addHandler(console)
+        myLogger = myutils.setup_logging(args.logfile)
     else:
-        logging.basicConfig(format='%(levelname)s:%(message)s',level=logging.DEBUG)
+        myLogger = myutils.setup_logging()
+    myLogger.getLogger('matplotlib.font_manager').disabled = True
+    
     
     ## basic command to run sxdefectalign2d
     command = ['~/sxdefectalign2d', '--vasp',
@@ -39,12 +43,14 @@ if __name__ == '__main__':
                '--vref', args.vref,
                '--vdef', args.vdef]
     
+    
     ## initialize the range of shift values bracketing the optimal shift
     smin, smax = -np.inf, np.inf
     shift = 0.0
     shifting = 'right'
     done = False
     counter = -1
+
 
     time0 = time.time()
     while not done and counter < 20:
@@ -57,48 +63,7 @@ if __name__ == '__main__':
         ## z  V^{model}  \DeltaV^{DFT}  V^{sr}
         data = np.loadtxt(args.vfile)
         
-        ## assumes that the slab is in the center of the cell vertically!
-        ## select datapoints corresponding to 2 bohrs at the top and bottom
-        ## of the supercell (i.e. a total of 4 bohrs in the middle of vacuum)
-        z1 = np.min([i for i,z in enumerate(data[:,0]) if z > 2.])
-        z2 = np.min([i for i,z in enumerate(data[:,0]) if z > (data[-1,0]-2.)])
-        
-        ## fit straight lines through each subset of datapoints
-        m1,C1 = np.polyfit(data[:z1,0],data[:z1,-1],1)
-        m2,C2 = np.polyfit(data[z2:,0],data[z2:,-1],1)
-        logging.debug("Slopes: %.8f %.8f; Intercepts: %.8f %.8f"%(m1,m2,C1,C2))
-        
-        ## check the slopes and intercepts of the lines
-        ## and shift the charge along z until the lines are flat
-        if abs(m1) < 1e-3 and abs(m2) < 1e-3 and abs(C1-C2) < 1e-3:
-            done = True
-            break
-        elif m1*m2 < 0:
-            logging.info("undetermined...make a tiny shift and try again")
-            if shifting == 'right':
-                shift += 0.01
-            else: 
-                shift -= 0.01
-            logging.info("try shift = %.8f"%shift)
-        elif (m1+m2)*np.sign(args.q) > 0:
-            smin = shift
-            if smax == np.inf:
-                shift += 1.0
-            else:
-                shift = (smin+smax)/2.0
-            shifting = 'right'
-            logging.debug("optimal shift is in [%.8f, %.8f]"%(smin,smax))
-            logging.info("shift charge in +z direction; try shift = %.8f"%shift)
-        elif (m1+m2)*np.sign(args.q) < 0:
-            smax = shift
-            if smin == -np.inf:
-                shift -= 1.0
-            else:
-                shift = (smin+smax)/2.0
-            shifting = 'left'
-            logging.debug("optimal shift is in [%.8f, %.8f]"%(smin,smax))
-            logging.info("shift charge in -z direction; try shift = %.8f"%shift)
-        
+        ## plot potential profiles
         if not args.noplot:
             plt.figure()
             plt.plot(data[:,0],data[:,2],'r',label=r'$V_{def}-V_{bulk}$')
@@ -108,19 +73,67 @@ if __name__ == '__main__':
             plt.ylabel("potential (eV)")
             plt.xlim(data[0,0],data[-1,0])
             plt.legend() 
-#            plt.show()
             plt.savefig(os.getcwd()+'/alignment.png')
+        
+        ## assumes that the slab is in the center of the cell vertically!
+        ## select datapoints corresponding to 2 bohrs at the top and bottom
+        ## of the supercell (i.e. a total of 4 bohrs in the middle of vacuum)
+        z1 = np.min([i for i,z in enumerate(data[:,0]) if z > 2.])
+        z2 = np.min([i for i,z in enumerate(data[:,0]) if z > (data[-1,0]-2.)])
+        
+        ## fit straight lines through each subset of datapoints
+        m1,C1 = np.polyfit(data[:z1,0],data[:z1,-1],1)
+        m2,C2 = np.polyfit(data[z2:,0],data[z2:,-1],1)
+        myLogger.debug("Slopes: %.8f %.8f; Intercepts: %.8f %.8f"%(m1,m2,C1,C2))
+        
+        ## check the slopes and intercepts of the lines
+        ## and shift the charge along z until the lines are flat
+        if (abs(m1) < args.threshold_slope and abs(m2) < args.threshold_slope
+            and abs(C1-C2) < args.threshold_C):
+            done = True
+            break
+        elif m1*m2 < 0:
+            myLogger.info("undetermined...make a tiny shift and try again")
+            if shifting == 'right':
+                shift += 0.01
+            else: 
+                shift -= 0.01
+            myLogger.info("try shift = %.8f"%shift)
+        elif (m1+m2)*np.sign(args.q) > 0:
+            smin = shift
+            if smax == np.inf:
+                shift += 1.0
+            else:
+                shift = (smin+smax)/2.0
+            shifting = 'right'
+            myLogger.debug("optimal shift is in [%.8f, %.8f]"%(smin,smax))
+            myLogger.info("shift charge in +z direction; try shift = %.8f"%shift)
+        elif (m1+m2)*np.sign(args.q) < 0:
+            smax = shift
+            if smin == -np.inf:
+                shift -= 1.0
+            else:
+                shift = (smin+smax)/2.0
+            shifting = 'left'
+            myLogger.debug("optimal shift is in [%.8f, %.8f]"%(smin,smax))
+            myLogger.info("shift charge in -z direction; try shift = %.8f"%shift)
+    
                        
     if done:
         C_ave = (C1+C2)/2
-        logging.info("DONE! shift = %.8f & alignment correction = %.8f"%(shift,C_ave))
+        myLogger.info("DONE! shift = %.8f & alignment correction = %.8f"%(shift,C_ave))
         ## run sxdefectalign2d with --shift <shift> -C <C_ave> > correction
         command2 = command + ['--shift', str(shift),
                               '-C', str(C_ave),
                               '> correction']
         os.system(' '.join(command2))
     else:
-        logging.info("Could not find optimal shift after 20 tries :(")
+        myLogger.info("Could not find optimal shift after 20 tries :(")
     
-    logging.debug("Total time taken (s): %.2f"%(time.time()-time0))
+    myLogger.debug("Total time taken (s): %.2f"%(time.time()-time0))
+    
+    
+if __name__ == '__main__':
+    
+    main(sys.argv[1:]) 
     
