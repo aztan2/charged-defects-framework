becccd301c51ee19cbf4b366a211ef274a5f2c75
Anne Marie Tan <annemarietan@ufl.edu>
2020-03-13 16:46:19 -0400

    Add option to directly provide eps_slab and d_slab.

--- apply_correction_2d.py
+++ apply_correction_2d.py
@@ -13,10 +13,17 @@ def main(args):
                                      to all charged defect calulations.')
     parser.add_argument('path_def', help='path to main defect directory')
     parser.add_argument('path_ref', help='path to the undefected reference directory')
-    parser.add_argument('file_dbentry',help='path to the relevant dbentry.json file')
-    parser.add_argument('--functional',help='functional that was used for this set of calculations', default='PBE')
-    parser.add_argument('--soc',help='whether or not to look in soc(dos) subdirectory',default=False,action='store_true')
-    parser.add_argument('--logfile',help='logfile to save output to')
+    parser.add_argument('--eps_slab', type=float,
+                        help='average slab dielectric constant (supply this or --file_dbentry)')
+    parser.add_argument('--d_slab', type=float,
+                        help='slab thickness (supply this or --file_dbentry)')
+    parser.add_argument('--file_dbentry', help='path to the relevant dbentry.json file \
+                        (supply this or --eps_slab and --d_slab)')
+    parser.add_argument('--functional',  default='PBE',
+                        help='functional that was used for this set of calculations')
+    parser.add_argument('--soc', default=False,action='store_true',
+                        help='whether or not to look in soc(dos) subdirectory')
+    parser.add_argument('--logfile', help='logfile to save output to')
        
     ## read in the above arguments from command line
     args = parser.parse_args()
@@ -28,18 +35,28 @@ def main(args):
     else:
         myLogger = myutils.setup_myLogger()
 
+    
+    ## if eps_slab and d_slab are directly provided
+    if args.eps_slab and args.d_slab:
+        eps_slab = args.eps_slab
+        d_slab = args.d_slab
+        myLogger.info("eps_slab: %.2f ; d_slab: %.2f"%(eps_slab,d_slab))
 
-    ## extract the eps_slab and d_slab from the relevant dbentry file
-    if os.path.exists(args.file_dbentry):
-        myLogger.info("Using slab properties from " + args.file_dbentry)
-        with open(args.file_dbentry, 'r') as file:
-            mater = json.loads(file.read())
-            eps_slab = mater[args.functional]["unitcell"]["eps_ave"]
-            d_slab = mater[args.functional]["unitcell"]["d_slab"]
-            myLogger.info("eps_slab: %.2f ; d_slab: %.2f"%(eps_slab,d_slab))
+    elif args.file_dbentry:
+        ## extract the eps_slab and d_slab from the relevant dbentry file
+        if os.path.exists(args.file_dbentry):
+            myLogger.info("Using slab properties from " + args.file_dbentry)
+            with open(args.file_dbentry, 'r') as file:
+                mater = json.loads(file.read())
+                eps_slab = mater[args.functional]["unitcell"]["eps_ave"]
+                d_slab = mater[args.functional]["unitcell"]["d_slab"]
+                myLogger.info("eps_slab: %.2f ; d_slab: %.2f"%(eps_slab,d_slab))
+        else:
+            ## if can't find a dbentry file
+            myLogger.info("Cannot find the file " + args.file_dbentry)
+            
     else:
-        ## if can't find a dbentry file
-        myLogger.info("Cannot find the file " + args.file_dbentry)
+        myLogger.info("Insufficient information provided about the slab dielectric profile")
 
 
     qs = myutils.listdironly(args.path_def)
