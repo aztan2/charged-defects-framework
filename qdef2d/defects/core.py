d3791edb0f860baf3a8933b23917f77d30efd3c2
Anne Marie Tan <annemarietan@ufl.edu>
2020-12-18 21:17:23 +0800

    Update function for parsing of initdef file.

--- qdef2d/defects/core.py
+++ qdef2d/defects/core.py
@@ -1,10 +1,5 @@
 import itertools
 import numpy as np
-import warnings
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    import pymatgen
-from pymatgen.io.vasp.inputs import Poscar
 from pymatgen.core.sites import PeriodicSite
 
 
@@ -23,47 +18,74 @@ class Defect():
    
     def parse_initdef(self, initdef):
         
+        """
+        Checks if the initdef json file contains the required fields 
+        and correct entry types.
+        
+        """
+        
         if "type" not in initdef.keys() or initdef["type"][0] not in ["v","i","s","a"]:
             raise ValueError ("unfamiliar defect type specified in initdef")
             return False
             
-        elif initdef["type"][0] == "v":
-            ## vacancy defect type requires the following fields:
-            for field,typ in zip(["species","index"],[str,int]):
-                if field not in initdef.keys():
-                    raise ValueError ("missing %s entry in initdef"%field)
-                    return False
-                elif not isinstance(initdef[field],typ):
-                    raise ValueError ("wrong entry type for %s in initdef"%field)
-                    return False
-            for field in ["index_offset_basis","index_offset_a","index_offset_b"]:
-                if field in initdef.keys():
-                    if not isinstance(initdef[field],int):
-                        raise ValueError ("wrong entry type for %s in initdef"%field)
-                        return False
-        """                
-        elif initdef["type"][0] == "i":                        
-            ## interstitial defect type requires the following fields:
-            for field,typ in zip(["species","index"],[str,int]):
+        elif initdef["type"][0] == "v" or initdef["type"][0] == "s":
+            ## vacancy/substitutional defect types require the following fields:
+            for field,typ in zip(["index","species"],[int,str]):
                 if field not in initdef.keys():
                     raise ValueError ("missing %s entry in initdef"%field)
                 elif not isinstance(initdef[field],typ):
                     raise ValueError ("wrong entry type for %s in initdef"%field)
+            ## and the following optional fields:    
             for field in ["index_offset_basis","index_offset_a","index_offset_b"]:
-                if field in initdef.keys():
-                    if not isinstance(initdef[field],int):
-                        raise ValueError ("wrong entry type for %s in initdef"%field)
-            
-                
-        
-        elif initdef["type"][0] == "i" or initdef["type"][0] == "a":
+                ## if not specified, assign a value of zero
+                initdef[field] = initdef.get(field,0)
+                if not isinstance(initdef[field],int):
+                    raise ValueError ("wrong entry type for %s in initdef"%field)
+            ## substitutional defect type requires an additional field:
+            if initdef["type"][0] == "s":                
+                if "species_new" not in initdef.keys():
+                    raise ValueError ("missing species_new entry in initdef")
+                elif not isinstance(initdef["species_new"],str):
+                    raise ValueError ("wrong entry type for species_new in initdef")
+
+        elif initdef["type"][0] == "i" or initdef["type"][0] == "a" :                        
+            ## interstitial/adatom defect types require the following fields:
+            for field,typ in zip(["species_new"],[str]):
+                if "species_new" not in initdef.keys():
+                    raise ValueError ("missing %s entry in initdef"%field)
+                elif not isinstance(initdef[field],typ):
+                    raise ValueError ("wrong entry type for %s in initdef"%field)
+            for field,typ in zip(["index","species"],[int,str]):
+                if field not in initdef.keys():
+                    raise ValueError ("missing %s entry in initdef"%field)
+                elif not isinstance(initdef[field],list):
+                    raise ValueError ("%s field in initdef needs to be a list"%field)
+                else:
+                    for entry in initdef[field]:
+                        if not isinstance(entry,typ):
+                            raise ValueError ("wrong entry type for %s in initdef"%field)                            
             if len(initdef["index"]) != len(initdef["species"]):
-                raise ValueError ("inconsistency between index and species lists")
-            for offset in ["index_offset_basis","index_offset_a","index_offset_b"]:
-                if initdef[offset] == 0:
-                    initdef[offset] = [0]*len(initdef["index"])
-                    """
+                raise ValueError ("index and species lists not of same length")
+            ## and the following optional fields:    
+            for field in ["index_offset_basis","index_offset_a","index_offset_b"]:
+                ## if not specified, assign a value of zero
+                initdef[field] = initdef.get(field,[0]*len(initdef["index"]))
+                if not isinstance(initdef[field],list):
+                    raise ValueError ("%s field in initdef needs to be a list"%field)
+                else:
+                    for entry in initdef[field]:
+                        if not isinstance(entry,int):
+                            raise ValueError ("wrong entry type for %s in initdef"%field)                            
+                if len(initdef[field]) != len(initdef["index"]):
+                    raise ValueError ("inconsistency in %s list length"%field)    
+            for field in ["shift_x","shift_y","shift_z"]:
+                ## if not specified, assign a value of zero
+                initdef[field] = initdef.get(field,0.)
+                if not isinstance(initdef[field],float):
+                    raise ValueError ("wrong entry type for %s in initdef"%field)
+        
         return initdef
+    
     
     def get_site_ind(self, site_sp, rel_ind, offset_basis_atom, offset_a, offset_b):
         
@@ -97,8 +119,8 @@ class Defect():
         ## evaluate the absolute site index
         ## I don't love this clunky method of specifying sites
         ## but I have yet to figure out a better way to do it... :(
-        siteind = (rel_ind + offset_basis_atom*self.supercell[0]*self.supercell[1]
-                           + offset_a*self.supercell[1] + offset_b)
+        siteind = (rel_ind + offset_basis_atom * self.supercell[0] * self.supercell[1]
+                           + offset_a * self.supercell[1] + offset_b)
         if siteind < 0:
             return(int(elem_minmax[site_sp][1]+siteind))
         else:
@@ -121,16 +143,9 @@ class Defect():
             defect_site = self.structure_bulk[siteind]
 
         
-        ## for interstitials/adatoms, the defect site coords are defined by
-        ## the center (average) of the user-provided list of sites
+        ## for interstitials/adatoms, the defect site coords are defined
+        ## by the center (average) of the user-provided list of sites
         elif initdef["type"][0] == "i" or initdef["type"][0] == "a":
-            if len(initdef["index"]) != len(initdef["species"]):
-                raise ValueError ("inconsistency between index and species lists")
-            for offset in ["index_offset_basis","index_offset_a","index_offset_b"]:
-                if initdef[offset] == 0:
-                    initdef[offset] = [0]*len(initdef["index"])
-
-            ## get absolute site indices
             siteind = [self.get_site_ind(sp,ind,offset_basis,offset_a,offset_b)
                            for sp,ind,offset_basis,offset_a,offset_b
                                in zip(initdef["species"],
@@ -142,30 +157,27 @@ class Defect():
             
             ## get the averaged position
             coords_ref = self.structure_bulk[siteind[0]].frac_coords
-            print (coords_ref)
+#            print (coords_ref)
             defect_coords = coords_ref.copy()
             for i in siteind[1:]:
                 coords = self.structure_bulk[i].frac_coords
-                print (coords,coords_ref,np.array(coords[0:3])-np.array(coords_ref[0:3]))
+#                print (coords,coords_ref,np.array(coords[0:3])-np.array(coords_ref[0:3]))
                 for j in [0,1,2]:
-                    if abs(coords[j] - coords_ref[j]) > 0.501:  ## dealing with pbc 
-                        if coords[j] > coords_ref[j]: coords[j] -= 1.0
-                        else: coords[j] += 1.0
-                print (coords)
+                    ## dealing with pbc 
+                    if (coords[j] - coords_ref[j]) > 0.501:
+                        coords[j] -= 1.0
+                    elif (coords_ref[j] - coords[j]) > 0.501:
+                        coords[j] += 1.0
+#                print (coords)
                 defect_coords += coords
             defect_coords = defect_coords/len(siteind)
-            print (defect_coords)
+#            print (defect_coords)
 
-            ## we may want to further shift the x-y position manually
+            ## we may want to further shift the position manually
             M = self.structure.lattice.matrix
-            if initdef.get("shift_x") != None:
-                defect_coords += float(initdef["shift_x"]) * np.linalg.inv(M)[0,:]
-            if initdef.get("shift_y") != None:
-                defect_coords += float(initdef["shift_y"]) * np.linalg.inv(M)[1,:]
-
-            ## we may want to shift the z position, e.g. in the case of an adatom            
-            if initdef.get("shift_z") != None:
-                defect_coords += float(initdef["shift_z"]) * np.linalg.inv(M)[2,:]
+            defect_coords += np.dot(np.linalg.inv(M).T,[initdef["shift_x"],
+                                                        initdef["shift_y"],
+                                                        initdef["shift_z"]])
             
             ## create the defect_site as a PeriodicSite object
             defect_site = PeriodicSite(initdef["species_new"],defect_coords,
